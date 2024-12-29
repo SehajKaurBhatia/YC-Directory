e2764770c2d030103bb41b0566f3fb8bc7b8f388
SehajKaurBhatia <sebhat1504@gmail.com>
2024-12-29 15:44:48 +0530

    first commit

---
+++ components/Search.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Form from "next/form"
+import SearchFormReset from './SearchFormReset';
+import Image from 'next/image';
+import { IoExitOutline } from "react-icons/io5";
+import { SearchIcon } from 'lucide-react';
+//form is used to use client side components on server side
+const Search = ({query}:{query?:string}) => {
+    
+   
+  return ( 
+  
+    <Form action="/" scroll={false} className='search-form'> 
+       <input
+       name="query"
+       defaultValue={query}
+       className='search-input'
+       placeholder="Search Startups"/>
+      <div className='flex gap-2 items-center'>
+        {query && <SearchFormReset/>}
+        <button type="submit" className='search-btn text-white'>
+        {/* <IoExitOutline  className="flex ml-1 self-center text-white"/> */}
+     <SearchIcon className='size-5'/>
+ </button>
+      </div>
+    </Form>
+ 
+  )
+}
+
+export default Search
